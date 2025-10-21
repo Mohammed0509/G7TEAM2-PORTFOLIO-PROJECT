0d468e1890187f0bd88584d580f630a9fb2cd5be
Mohammed0509 <mohammed.khaled@ensia.edu.dz>
2025-10-21 02:03:10 -1100

    previous branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwind from "tailwindcss";
+import { defineConfig } from "vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: "./static",
-  base: "/G7TEAM2-PORTFOLIO-PROJECT/", // change to "/G7TEAM2-PORTFOLIO-PROJECT/" if deploying to GitHub Pages
+  base: "./",
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
   },
 });
-
